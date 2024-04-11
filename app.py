ada23dada373ea24520f9e33236c72990b977f5b
topaccina <54760952+topaccina@users.noreply.github.com>
2024-04-11 22:32:13 +0200

    updated main app file, design homepage added custom style and homepages pngs

--- app.py
+++ app.py
@@ -1,25 +1,84 @@
-# This file is typically the entry page to a multipage app using Dash Page -- https://dash.plotly.com/urls#dash-pages
-# More multipage app examples by Ann Marie: https://github.com/AnnMarieW/dash-multi-page-app-demos
-# Here is some code to get started:
-
-import dash
-from dash import Dash, html, dcc
+# import pandas as pd
+from dash import Dash, html, page_container
 import dash_bootstrap_components as dbc
+import dash
 
-app = Dash(__name__,
-           use_pages=True,
-           suppress_callback_exceptions=True,
-           external_stylesheets=[dbc.themes.YETI, dbc.icons.BOOTSTRAP])
+app = Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.SPACELAB, dbc.icons.BOOTSTRAP],
+    use_pages=True,
+    pages_folder="pages",
+    prevent_initial_callbacks=True,
+)
 
-app.layout = html.Div([
-    html.H1("Response Reporting Platform -- VOST Europe"),
-    html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
-    ]),
-    dash.page_container
-])
+########### Navbar design section####################
+# dropdown w/ quick links to navigate to the other pages
+quickLinksLabels = {
+    "Homepage": "Home",
+    "Data-insights": "Data Insights",
+    "Application": "Apply to Volunteer",
+    # "Internal": "Internal",
+}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+nav = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem(quickLinksLabels[page["name"]], href=page["path"])
+        for page in dash.page_registry.values()
+        if (page["module"] != "pages.not_found_404") & (page["name"] != "Internal")
+    ],
+    nav=True,
+    in_navbar=True,
+    label="Quick Links",
+    className="me-5 text-primary fw-bold",
+)
+# assembly the navbar
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Img(
+                                    src=dash.get_asset_url("VOST_LOGO.png"),
+                                    height="30px",
+                                ),
+                                html.Img(
+                                    src=dash.get_asset_url("VOST_EU.png"), height="30px"
+                                ),
+                            ],
+                            className="me-2 text-primary",
+                        ),
+                        dbc.Col(
+                            dbc.NavbarBrand(
+                                "Response Reporting Platform",
+                                className="ms-2 text-primary",
+                            )
+                        ),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+            ),
+            nav,
+        ]
+    ),
+    dark=True,
+    className="opacity-100 p-2  text-white fw-bold rounded",
+)
+
+# page container
+content = html.Div(id="page-content", children=[page_container], className="content")
+
+# main app layout
+app.layout = dbc.Container(
+    [dbc.Row([dbc.Col([navbar, content], width=12)])],
+    fluid=False,
+    style={},
+    className="bg-opacity-10 p-2 bg-primary text-dark fw-bold rounded border border-light vh-100",
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- pages/homepage.py
+++ pages/homepage.py
@@ -1,12 +1,102 @@
-import dash
-import dash_mantine_components as dmc
-from dash import html
 import dash_bootstrap_components as dbc
+from dash import html, dcc
+import dash
 
-dash.register_page(__name__,path='/')
+dash.register_page(__name__, path="/", order=0)
 
-layout =  dbc.Container(
-    children=[
-        html.H1("Welcome to the VOST page"),
-    ]
+########### Accordion design section ####################
+# accordion items for each action - no direct access to the internal page
+accordion = html.Div(
+    [
+        html.H4(
+            "VOST Fight Against Disinformation",
+            className="text-primary fw-bold mt-3 ms-5 ",
+        ),
+        dbc.Accordion(
+            [
+                dbc.AccordionItem(
+                    [
+                        html.P(
+                            "Provide support in hoax and abusive behavior, disinformation and misinformation detection, by monitoring multiple channels and by establishing direct communication channels with online platforms"
+                        ),
+                        dcc.Link(
+                            "Learn more",
+                            href="https://vosteurope.org/",
+                            target="_blank",
+                        ),
+                    ],
+                    title="Core Mission",
+                    className="bg-opacity-10 me-5  ms-5  mt-3 border-primary border-1 bg-primary text-primary  rounded",
+                ),
+                dbc.AccordionItem(
+                    [
+                        html.P(
+                            "A registration process is needed to become a vetted user"
+                        ),
+                        # dbc.Button("Don't click me!", color="danger"),
+                        dcc.Link("Go to the application page", href="/application"),
+                    ],
+                    title="Apply to Volunteer",
+                    className="bg-opacity-10 me-5  ms-5  mt-3 border-primary border-1 bg-primary text-primary  rounded",
+                ),
+                dbc.AccordionItem(
+                    [
+                        html.P(
+                            "Analytics dashboard for trending reports, data drill-down, highlight outstanding issue"
+                        ),
+                        dcc.Link("Go to data insight page", href="/data-insights"),
+                    ],
+                    title="Data Insights",
+                    className="bg-opacity-10 me-5  ms-5  mt-3 mb-5 border-primary border-1 bg-primary text-primary  rounded",
+                ),
+            ],
+            start_collapsed=True,
+        ),
+    ],
+    className=" border border-primary border-1 text-primary fw-bold rounded ",
+)
+# assembly the home page layout - banner and accordion instantiation
+layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Container(
+                            html.Img(
+                                src=dash.get_asset_url("Banner.png"),
+                                style={
+                                    "margin-left": "auto",
+                                    "margin-right": "auto",
+                                    "display": "block",
+                                    "width": "100%",
+                                },
+                            ),
+                            className="p-2 ",
+                            fluid=False,
+                        ),
+                    ],
+                ),
+            ],
+            align="center",
+        ),
+        # html.Br(),
+        dbc.Row(
+            [
+                dbc.Col([], width=2),
+                dbc.Col(
+                    [
+                        dbc.Container(
+                            accordion,
+                            fluid=False,
+                        ),
+                    ],
+                    width=8,
+                ),
+                dbc.Col([], width=2),
+            ],
+            align="center",
+            className=" p-5",
+        ),
+    ],
 )
